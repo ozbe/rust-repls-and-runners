71e656fad4945e21dfd9fa9fddca806041400ce5
ozbe <1372945+ozbe@users.noreply.github.com>
2020-05-23 13:02:40 -0400

    fix formatting in runner rand-example

--- runner/rand-example.rs
+++ runner/rand-example.rs
@@ -2,9 +2,9 @@
 use rand::prelude::*;
 
 let mut rng = rand::thread_rng();
-  let y: f64 = rng.gen();
-  println!("y = {}", y);
+let y: f64 = rng.gen();
+println!("y = {}", y);
 
-  let mut nums: Vec<i32> = (1..100).collect();
-  nums.shuffle(&mut rng);
-  println!("nums = {:?}", nums);
+let mut nums: Vec<i32> = (1..100).collect();
+nums.shuffle(&mut rng);
+println!("nums = {:?}", nums);
